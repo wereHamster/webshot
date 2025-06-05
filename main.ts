01659e9f2eddb6bd924592f91cf485c609295b00
Tomas Carnecky <tomc@caurea.org>
2025-06-05 23:12:29 +0200

    .

--- main.ts
+++ main.ts
@@ -2,28 +2,31 @@ import { chromium, devices } from "npm:playwright@1.49.1";
 
 const port = parseInt(Deno.env.get("PORT") ?? "3000", 10);
 
-Deno.serve({ port }, async (req) => {
-  const [_, url] = new URL(req.url).pathname.match(
-    "/og/(.*)$",
-  )!;
+const browserPromise = chromium.launch({
+  args: ["--no-sandbox", "--disable-dev-shm-usage"],
+});
 
-  const browser = await chromium.launch({
-    args: ["--no-sandbox", "--disable-dev-shm-usage"],
-  });
-  const context = await browser.newContext(devices["Desktop Chrome HiDPI"]);
+Deno.serve({ port }, async (req) => {
+  const [_, url] = new URL(req.url).pathname.match("/og/(.*)$")!;
+
+  const browser = await browserPromise;
+
+  const context = await browser.newContext(devices["Desktop Chrome"]);
   const page = await context.newPage();
   await page.setViewportSize({
     width: 1200,
     height: 630,
   });
+
   await page.goto(`https://${url}`, { waitUntil: "networkidle" });
-  const image = (await page.screenshot({
+
+  const image = await page.screenshot({
     type: "jpeg",
     quality: 90,
     fullPage: true,
-  })) as Uint8Array;
+  });
 
-  await browser.close();
+  await context.close();
 
   return new Response(image, {
     status: 200,

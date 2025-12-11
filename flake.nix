008ec4717343052f0e6723af594982c3a560f90b
Tomas Carnecky <tomc@caurea.org>
2025-12-11 08:41:27 +0100

    Update deno

--- flake.nix
+++ flake.nix
@@ -4,20 +4,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in {
-          devShells.default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.deno
-              pkgs.google-cloud-sdk
-            ];
-          };
-        }
-      );
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.deno
+            pkgs.google-cloud-sdk
+          ];
+        };
+      }
+    );
 }

581fc80112f104c1868b7b0cfbf9458c67108f5e
Tomas Carnecky <tomc@caurea.org>
2026-04-18 17:06:24 +0200

    Connect with journald

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,29 @@ pub mod v1;
 use chromiumoxide::Browser;
 use dropshot::{endpoint, HttpError, HttpResponseOk, RequestContext};
 use std::sync::Arc;
+use tracing_subscriber::prelude::*;
+
+pub fn init_tracing_subscriber() {
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        tracing_subscriber::EnvFilter::new("info,chromiumoxide::handler=error")
+    });
+
+    // If we detect that stdout/stderr is connected to journald, use the
+    // journald-specific layer.
+    //
+    // If connecting to journald fails, fall through to the fmt subscriber.
+    if std::env::var("JOURNAL_STREAM").is_ok() {
+        if let Ok(journald_layer) = tracing_journald::layer() {
+            tracing_subscriber::registry()
+                .with(env_filter)
+                .with(journald_layer)
+                .init();
+            return;
+        }
+    }
+
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+}
 
 pub struct ServerContext {
     pub auth: auth::Auth,
